7171be42af2a6d2fb907e36f61eb3a84ebd43c67
Murad Kamali <murad.coding@gmail.com>
2024-04-25 10:04:25 +0100

    Fixes missing salary property for model class, implements override methods for Employee, updates tests

--- src/main/java/employees/Employee.java
+++ src/main/java/employees/Employee.java
@@ -1,6 +1,7 @@
 package employees;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Employee {
     private final int empId;
@@ -13,7 +14,9 @@ public class Employee {
     private final LocalDate dateOfBirth;
     private final LocalDate dateOfJoin;
 
-    public Employee(int empId, String prefix, String firstName, String lastName, String middleInitial, String gender, String email, LocalDate dateOfBirth, LocalDate dateOfJoin) {
+    private final int salary;
+
+    public Employee(int empId, String prefix, String firstName, String lastName, String middleInitial, String gender, String email, LocalDate dateOfBirth, LocalDate dateOfJoin, int salary) {
         this.empId = empId;
         this.prefix = prefix;
         this.firstName = firstName;
@@ -23,6 +26,36 @@ public class Employee {
         this.email = email;
         this.dateOfBirth = dateOfBirth;
         this.dateOfJoin = dateOfJoin;
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return empId == employee.empId && salary == employee.salary && Objects.equals(prefix, employee.prefix) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(middleInitial, employee.middleInitial) && Objects.equals(gender, employee.gender) && Objects.equals(email, employee.email) && Objects.equals(dateOfBirth, employee.dateOfBirth) && Objects.equals(dateOfJoin, employee.dateOfJoin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(empId, prefix, firstName, lastName, middleInitial, gender, email, dateOfBirth, dateOfJoin, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", prefix='" + prefix + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleInitial='" + middleInitial + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", dateOfJoin=" + dateOfJoin +
+                ", salary=" + salary +
+                '}';
     }
 
     public int getEmpId() {
@@ -59,5 +92,9 @@ public class Employee {
 
     public LocalDate getDateOfJoin() {
         return dateOfJoin;
+    }
+
+    public int getSalary() {
+        return salary;
     }
 }
--- src/test/java/employees/EmployeeDAOTests.java
+++ src/test/java/employees/EmployeeDAOTests.java
@@ -19,13 +19,13 @@ class EmployeeDAOTests {
         employees = new ArrayList<>();
         employees.add(new Employee(111111, "Mr", "Bob", "Jones", "M", "M",
                 "bob.jones@mail.com", LocalDate.of(1980, 6, 24),
-                LocalDate.of(2010, 9, 1)));
+                LocalDate.of(2010, 9, 1), 30000));
         employees.add(new Employee(222222, "Mrs", "Alice", "Smith", "C", "F",
                 "alice.smith@mail.com", LocalDate.of(1990, 11, 5),
-                LocalDate.of(2015, 10, 1)));
+                LocalDate.of(2015, 10, 1), 40000));
         employees.add(new Employee(111111, "Dr", "Eve", "Brown", "S", "F",
                 "eve.brown@mail.com", LocalDate.of(1976, 4, 15),
-                LocalDate.of(2005, 4, 1)));
+                LocalDate.of(2005, 4, 1), 50000));
         dao = new EmployeeDAO(employees);
     }
 
